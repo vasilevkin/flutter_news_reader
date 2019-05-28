6ab59cd054e68748236e38fb741dd29e402605c5
Sergey Vasilevkin <svasilev@mera.ru>
2019-05-28 08:59:55 +0300

    Update ui design for news item, add ExpansionTile and Read full button.

--- lib/widgets/NewsItem.dart
+++ lib/widgets/NewsItem.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_news_reader/DetailsPage.dart';
+
 class NewsItem extends StatelessWidget {
   final String sourceId;
   final String sourceName;
@@ -24,16 +26,71 @@ class NewsItem extends StatelessWidget {
       this.content})
       : super(key: key);
 
+  Widget _articleText(text) {
+    String formattedText = "n/a";
+    if (text != null) {
+      formattedText = text;
+    }
+    return Text(formattedText);
+  }
+
+  Widget _articleImage(imageUrl) {
+    if (imageUrl != null) {
+      return Image.network(imageUrl);
+    } else {
+      return Placeholder(
+        fallbackWidth: 100,
+        fallbackHeight: 100,
+      );
+    }
+  }
+
+  Widget _articleDate(String textDate) {
+    String formattedText = "n/a";
+    if (textDate != null) {
+      formattedText = textDate.substring(0, 10);
+    }
+    return Text(formattedText);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 5.0,
-      child: Column(
-        children: <Widget>[
-          Text(title),
-//          Text(author),
-          Text(description),
-        ],
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                _articleText(sourceName),
+                _articleDate(publishedAt),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                _articleText(title),
+                _articleImage(urlToImage),
+              ],
+            ),
+            _articleText(description),
+            ExpansionTile(
+              title: Text("more..."),
+              children: <Widget>[
+                _articleText(content),
+                RaisedButton(
+                    child: Text("Read full"),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => DetailsPage(url)));
+                    }),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
