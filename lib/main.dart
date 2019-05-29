7f82bfb49df2fee03417c5c8fc48034a5dd683d4
Sergey Vasilevkin <svasilev@mera.ru>
2019-05-29 09:09:58 +0300

    Update UI, make all fonts bigger.

--- lib/main.dart
+++ lib/main.dart
@@ -14,25 +14,25 @@ class NewsReaderApp extends StatelessWidget {
         textTheme: Theme.of(context).textTheme.copyWith(
               caption: TextStyle(color: Colors.white54),
               subhead: TextStyle(
-                fontSize: 10.0,
+                fontSize: 12.0,
               ),
               title: TextStyle(
-                fontSize: 14.0,
+                fontSize: 16.0,
                 fontStyle: FontStyle.italic,
                 fontWeight: FontWeight.bold,
               ),
               subtitle: TextStyle(
-                fontSize: 11.0,
+                fontSize: 13.0,
                 fontStyle: FontStyle.italic,
               ),
               body1: TextStyle(
-                fontSize: 11.0,
+                fontSize: 13.0,
               ),
               body2: TextStyle(
-                fontSize: 10.0,
+                fontSize: 12.0,
               ),
               button: TextStyle(
-                fontSize: 11.0,
+                fontSize: 13.0,
               ),
             ),
       ),
